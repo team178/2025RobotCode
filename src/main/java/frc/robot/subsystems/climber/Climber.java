900a4c1ec378b962013e07acc2ea04ac5897a59a
arnav <arnav.bholausa@gmail.com>
2025-02-24 17:57:32 -0500

    Added outline of Climber

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -1,9 +1,25 @@
 package frc.robot.subsystems.climber;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import org.littletonrobotics.junction.Logger;
-
 public class Climber extends SubsystemBase{
-    
+    private ClimberIO climberIO;
+
+    public Climber(ClimberIO climberIO) {
+        this.climberIO = climberIO;  
+    }
+
+    public Command toggleClimber(ClimberPosition climberPosition) {
+        return runOnce(null);
+    }
+
+    public Command runVolts(double volts) {
+        return runOnce(() -> climberIO.setVolts(volts));
+    }
+
+    @Override
+    public void periodic() {
+
+    }
 }
--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -2,12 +2,23 @@ package frc.robot.subsystems.climber;
 
 import org.littletonrobotics.junction.AutoLog;
 
-public interface ClimberIO{
+public interface ClimberIO {
     @AutoLog
-    public static class ClimberIOInputs {}
+    public static class ClimberIOInputs {
+        public ClimberPosition desiredPosition = ClimberPosition.HOME;
+
+        public double climberHeight = 0;
+        public double climberVelocity = 0;
+
+        public double elevatorAppliedVolts = 0;
+        public double elevatorCurrentAmps = 0;
+    }
     
     public default void updateInputs(ClimberIOInputs inputs) {}
 
-    public default void updateClimberPosition() {}
+    public default void setClimberPosition(ClimberPosition climberPosition) {}
     
+    public default void setVolts(double volts) {}
+
+    public default void updateControlConstants() {}
 }
