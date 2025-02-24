2263755bd440693e7f358480761d68cbce3999d1
arnav <arnav.bholausa@gmail.com>
2025-02-24 17:19:36 -0500

    Initializing Climber

--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -2,9 +2,12 @@ package frc.robot.subsystems.climber;
 
 import org.littletonrobotics.junction.AutoLog;
 
-public interface ClimberIO {
+public interface ClimberIO{
     @AutoLog
     public static class ClimberIOInputs {}
+    
+    public default void updateInputs(ClimberIOInputs inputs) {}
 
+    public default void updateClimberPosition() {}
     
 }
